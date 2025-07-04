4a8cba4f8532b58cfe17ff2f51d3b07417e3e176
neelcc <neel.c.chaurasiya@gmail.com>
2025-07-04 11:04:45 +0530

    css for phone

--- Client/src/pages/Result.jsx
+++ Client/src/pages/Result.jsx
@@ -25,14 +25,14 @@ const Result = () => {
     <form onSubmit={onSubmitHandler}  className=' flex flex-col min-h-[90vh] justify-center items-center py-4 ' action="">
       <div>
       <div className=' relative ' >
-        <img className=' max-w-sm rounded ' src={image} alt="" />
+        <img className=' max-w-sm rounded w-60 md:w-80 lg:w-85 ' src={image} alt="" />
         <span className= {` absolute bottom-0 left-0  bg-blue-500 h-1 ${ loading ?'w-full transition-all duration-[10s]' : 'w-0'} `} ></span> 
       </div>
         <p className={!loading ? 'hidden' : '' }  >Loading....</p>
-        </div>
-       { !isImageLoaded  && <div className='flex w-full max-w-2xl  text-white bg-neutral-500 rounded-full text-sm p-0.5 mt-10  ' >
-          <input onChange={ e => setInput(e.target.value)} value={input}  className=' flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color  ' type="text" placeholder='Describe what you want to generate'  />
-          <button type='submit' className=' bg-zinc-900 px-10 py-3 rounded-full sm:px-16 text-white ' > Generate</button>     
+        </div  >
+       { !isImageLoaded  && <div className=' flex  w-full max-w-2xl  text-white bg-neutral-500 rounded-full text-sm p-0.5 mt-10  ' >
+          <input onChange={ e => setInput(e.target.value)} value={input}  className=' text-xs  flex-1 bg-transparent outline-none ml-6 max-sm:w-20 placeholder-color   ' type="text" placeholder='Describe what you want to generate'  />
+          <button type='submit' className=' bg-zinc-900 lg:px-10 md:px-10 py-2 sm:py-3 md:py-3 lg:py-3 rounded-full sm:px-16 px-2 text-white ' > Generate</button>     
         </div>}
 
      { isImageLoaded && <div className='flex gap-2 flex-wrap items-center justify-center text-sm p-0.5 mt-10 rounded-full  ' >
--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -3,7 +3,7 @@ const userSchema = new mongoose.Schema({
     name : {type: String , required:true},
     email : {type: String , required:true, unique : true},
     password : {type:String, required:true},
-    creditBalance : {type:Number , default:50}
+    creditBalance : {type:Number , default:5}
 })
 
 const userModel = mongoose.model.user || mongoose.model("user",userSchema)
